139d35a74adc7191dbd12772813e7e2df1e3f4f8
Zexzill <farel_mohammad@yahoo.com>
2021-11-16 13:20:19 +0700

    init commit problem 7

---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject Box;
+    public Transform Canvas;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnBoxRandomly", 2, 2);
+    }
+
+    private void SpawnBoxRandomly()
+    {
+        GameObject spawnedBox = Instantiate(Box, Canvas);
+        RectTransform boxRectTransform = spawnedBox.GetComponent<RectTransform>();
+
+        int randomPositionX = Random.Range(-120, 120);
+        int randomPositionY = Random.Range(-120, 120);
+        int randomBoxSizeX = Random.Range(10, 25);
+        int randomBoxSizeY = Random.Range(10, 25);
+
+        Vector2 randomedPosition = new Vector2(randomPositionX, randomPositionY);
+        Vector2 randomedSize = new Vector2(randomBoxSizeX, randomBoxSizeY);
+
+        boxRectTransform.localPosition = randomedPosition;
+        boxRectTransform.sizeDelta = randomedSize;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text ScoreText;
+    private int score = 0;
+    public void IncreaseScore(int value)
+    {
+        ScoreText.text = $"skor {(score += value)}";
+    }
+}
