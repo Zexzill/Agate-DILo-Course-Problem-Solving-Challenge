0ba158efe2982f4c2e80e6eda3c2293c570f3fd2
Zexzill <farel_mohammad@yahoo.com>
2021-11-16 20:42:59 +0700

    init commit problem 8

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private string _circleTag = "circle";
+    private string _gameManagerTag = "manager";
+    private GameManager _gameManager;
+
+    private void Start()
+    {
+        GameObject targetObj = GameObject.FindGameObjectWithTag(_gameManagerTag);
+        _gameManager = targetObj.GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.CompareTag(_circleTag))
+        {
+            _gameManager.IncreaseScore(1);
+            gameObject.SetActive(false);
+        }
+    }
+}
